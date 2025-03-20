a59dac255b5c04a7527ecde2ef0ded9d6c3f8dc4
alain <79479952+chadnpc@users.noreply.github.com>
2025-03-20 02:13:18 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 ﻿
 #!/usr/bin/env pwsh
 # .SYNOPSIS
-#   devConstants buildScript v0.1.0
+#   devConstants buildScript v0.1.1
 # .DESCRIPTION
 #   A custom build script for the module devConstants
 # .LINK
